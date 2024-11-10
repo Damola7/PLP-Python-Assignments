e45951d5fe51186afe1d0f7bcfcf2648d60c2fef
Damola7 <jubrilshotayo7@gmail.com>
2024-11-10 17:56:07 +0100

    completed the personalized greeting app weekly challenge

---
+++ Weekly-Code-Challenges/greeting_app.py
@@ -0,0 +1,7 @@
+
+# Ask the user for their name and favorite color
+name = input("What's your name? ")
+color = input("What's your favorite color? ")
+
+# Print a personalized greeting
+print(f"Hello, {name}! Your favorite color {color}, is awesome!")
