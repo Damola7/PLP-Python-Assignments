8276b1a0a81a6a9984709e33a06ded6ff2be6688
Damola7 <jubrilshotayo7@gmail.com>
2024-11-21 10:12:43 +0100

    week 2 weekly challenge number 2: Completed

---
+++ Weekly-Code-Challenges/fav-book.py
@@ -0,0 +1,6 @@
+# Creating a tuple of favorite books
+favorite_books = ("AOT", "Tales of Demons and Gods", "Strategize to Win", "The Trading Bible")
+
+# Printing each book name on a separate line
+for book in favorite_books:
+    print(book)
